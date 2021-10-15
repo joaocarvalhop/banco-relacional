89e693c220067f25f35f1ed547ecf379531552a4
joaocarvalhop <jppcarvalho8@gmail.com>
2021-10-15 19:54:03 -0300

    Atualizar Estados

---
+++ atualizarEstados.sql
@@ -0,0 +1,15 @@
+-- Colocar ~ no Maranhão
+UPDATE estados
+SET nome = 'Maranhão'
+WHERE sigla = 'MA'
+
+SELECT est.nome FROM estados est WHERE sigla = 'MA';
+
+UPDATE estados
+SET nome = 'Paraná', 
+    populacao = 11.32
+WHERE sigla = 'PR'
+
+SELECT est.nome 
+FROM estados est 
+WHERE sigla = 'PR';
