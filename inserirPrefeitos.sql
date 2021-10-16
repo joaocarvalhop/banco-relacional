2d373af80869f48e4d658aaf2b61410a78357d6b
joaocarvalhop <jppcarvalho8@gmail.com>
2021-10-16 10:36:32 -0300

    Inserir Prefeitos

---
+++ inserirPrefeitos.sql
@@ -0,0 +1,12 @@
+INSERT INTO prefeitos 
+    (nome, cidade_id)
+VALUES
+    ('Shalom mazel tov', 1),
+    ('Rodrigo Neves', 2),
+    ('Jucelino Lampião', 3),
+    ('Ciro Gomes', 4);
+
+INSERT INTO prefeitos 
+    (nome, cidade_id)
+VALUES
+    ('Gang gang', null);
